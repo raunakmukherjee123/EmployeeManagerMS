9fcd782b24a7372aafd5146968d85219de43358c
Raunak <rounand44@gmail.com>
2026-08-03 15:06:47 +0530

    created project

---
+++ EmployeeMS/EmployeeMS/src/main/java/com/example/EmployeeMS/models/Address.java
@@ -0,0 +1,5 @@
+package com.example.EmployeeMS.models;
+
+public class Address extends BaseEntity{
+
+}
---
+++ EmployeeMS/EmployeeMS/src/main/java/com/example/EmployeeMS/models/Employee.java
@@ -0,0 +1,18 @@
+package com.example.EmployeeMS.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employee")
+public class Employee extends BaseEntity{
+
+}
